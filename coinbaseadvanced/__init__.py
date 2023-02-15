bc736a6399c443530f6bb02e546a844ce024fb46
Camilo Quintas <kmiloc89@gmail.com>
2023-02-15 00:15:44 -0500

    packaging

---
+++ coinbaseadvanced/__init__.py
@@ -0,0 +1,4 @@
+"""
+Package Version
+"""
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,40 @@
+"""
+Package Setup Configurations.
+"""
+
+from setuptools import setup, find_packages
+import coinbaseadvanced
+
+requirements = [
+    'requests>=2.28.1',
+    'requests-toolbelt>=0.10.1',
+    'typed-ast>=1.5.4',
+    'typing-extensions>=4.4.0',
+]
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme = fh.read()
+
+setup(
+    name='coinbaseadvanced',
+    version=coinbaseadvanced.__version__,
+    packages=find_packages(),
+    license='MIT',
+    description='Coinbase Advanced Trade API client library.',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url='https://github.com/KmiQ/coinbase-advanced-python/',
+    author='Camilo Quintas',
+    author_email='valleycryptostreet@gmail.com',
+    keywords=['api', 'coinbase', 'bitcoin', 'client'],
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+)
